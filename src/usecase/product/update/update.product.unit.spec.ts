9a5635dc1fa70c12e49db9df9827f108c483043c
Everton Zoelner <everton.zoelner@gmail.com>
2024-08-08 20:50:01 -0300

    feat: adiciona usecase 'update product'

---
+++ src/usecase/product/update/update.product.unit.spec.ts
@@ -0,0 +1,34 @@
+import ProductFactory from "../../../domain/product/factory/product.factory";
+import ProductUpdateUseCase from "./update.product.usecase";
+
+function makeSut() {
+  const productRepository = {
+    create: jest.fn(),
+    update: jest.fn(),
+    find: jest.fn(),
+    findAll: jest.fn(),
+  };
+
+  const productUpdateUseCase = new ProductUpdateUseCase(productRepository);
+  return { usecase: productUpdateUseCase, repository: productRepository };
+}
+
+describe("Unit test for updating product use case", () => {
+  it("should update a product", async () => {
+    const { usecase, repository } = makeSut();
+
+    const product = ProductFactory.create("a", "Product A", 100);
+
+    repository.find.mockResolvedValueOnce(product);
+
+    const output = await usecase.execute({
+      id: product.id,
+      name: "Product A Updated",
+      price: 200,
+    });
+
+    expect(output.id).toBe(product.id);
+    expect(output.name).toBe("Product A Updated");
+    expect(output.price).toBe(200);
+  });
+});
