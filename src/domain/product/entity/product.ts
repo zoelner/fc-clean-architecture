38b07c84aceb782a7f820eb4d8d0606e14c1a784
Everton Zoelner <everton.zoelner@gmail.com>
2024-09-02 22:54:05 -0300

    feat: adiciona suporte a notificação na entidade product

--- src/domain/product/entity/product.ts
+++ src/domain/product/entity/product.ts
@@ -1,21 +1,22 @@
+import Entity from "../../@shared/entity/entity.abstract";
+import NotificationError from "../../@shared/notification/notification.error";
 import ProductInterface from "./product.interface";
 
-export default class Product implements ProductInterface {
-  private _id: string;
+export default class Product extends Entity implements ProductInterface {
   private _name: string;
   private _price: number;
 
   constructor(id: string, name: string, price: number) {
+    super();
     this._id = id;
     this._name = name;
     this._price = price;
     this.validate();
+    if (this.notification.hasErrors()) {
+      throw new NotificationError(this.notification.getErrors());
+    }
   }
 
-  get id(): string {
-    return this._id;
-  }
-  
   get name(): string {
     return this._name;
   }
@@ -35,14 +36,23 @@ export default class Product implements ProductInterface {
   }
 
   validate(): boolean {
-    if (this._id.length === 0) {
-      throw new Error("Id is required");
+    if (this.id.length === 0) {
+      this.notification.addError({
+        message: "Id is required",
+        context: "product",
+      });
     }
     if (this._name.length === 0) {
-      throw new Error("Name is required");
+      this.notification.addError({
+        message: "Name is required",
+        context: "product",
+      });
     }
     if (this._price < 0) {
-      throw new Error("Price must be greater than zero");
+      this.notification.addError({
+        message: "Price must be greater than zero",
+        context: "product",
+      });
     }
     return true;
   }
