914156f57e13ca34a19240cc9c216f11d4aaf060
Everton Zoelner <everton.zoelner@gmail.com>
2024-09-04 20:26:09 -0300

    feat: adiciona suporte a validação na entidade product

--- src/domain/product/entity/product.ts
+++ src/domain/product/entity/product.ts
@@ -1,5 +1,6 @@
 import Entity from "../../@shared/entity/entity.abstract";
 import NotificationError from "../../@shared/notification/notification.error";
+import ProductValidatorFactory from "../factory/product.validator.factory";
 import ProductInterface from "./product.interface";
 
 export default class Product extends Entity implements ProductInterface {
@@ -35,25 +36,7 @@ export default class Product extends Entity implements ProductInterface {
     this.validate();
   }
 
-  validate(): boolean {
-    if (this.id.length === 0) {
-      this.notification.addError({
-        message: "Id is required",
-        context: "product",
-      });
-    }
-    if (this._name.length === 0) {
-      this.notification.addError({
-        message: "Name is required",
-        context: "product",
-      });
-    }
-    if (this._price < 0) {
-      this.notification.addError({
-        message: "Price must be greater than zero",
-        context: "product",
-      });
-    }
-    return true;
+  validate() {
+    ProductValidatorFactory.create().validate(this);
   }
 }
